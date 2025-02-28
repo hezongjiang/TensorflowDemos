14bda744172e990226d95c5be9c52f68953879c4
hezongjiang <422736262@qq.com>
2025-02-28 18:19:27 +0800

    Update classify_images_number.py

--- classify_images_number.py
+++ classify_images_number.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # load data
-datapath = '/data/mnist/mnist.npz'
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data(datapath)
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
 # show first image
 plt.imshow(train_images[0])
@@ -17,10 +16,11 @@ test_images = tf.keras.utils.normalize(test_images, axis=1)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# 使用 Keras Model.compile 配置和编译模型
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
 # training
 model.fit(train_images, train_labels, epochs=3)
@@ -30,10 +30,11 @@ print(val_loss)
 print(val_acc)
 
 # save model
-model.save('epic_num_reader.model')
+model_name = 'epic_num_reader.model'
+model.save(model_name)
 
 # load model
-new_model = tf.keras.models.load_model('epic_num_reader.model')
+new_model = tf.keras.models.load_model(model_name)
 
 # show some one
 i = 1000
